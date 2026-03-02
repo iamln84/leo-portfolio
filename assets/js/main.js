e2f705d5b5ad24be30874d60ec0bd54356f3a7fa
Leo <98237426+iamln84@users.noreply.github.com>
2026-03-01 22:32:43 -0500

    Implement back to top button visibility on scroll
    Add functionality to show/hide 'back to top' button on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
+const backToTop = document.getElementById("backToTop");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
